4bb59b327725a8a454e406a6bc76cfb55561e3e3
zclfly <zclfly@yahoo.com>
2016-03-26 23:02:20 +0800

    flow2vec initial version

--- gensim/models/__init__.py
+++ gensim/models/__init__.py
@@ -11,6 +11,7 @@ from .tfidfmodel import TfidfModel
 from .rpmodel import RpModel
 from .logentropy_model import LogEntropyModel
 from .word2vec import Word2Vec
+from .flow2vec import Flow2Vec
 from .doc2vec import Doc2Vec
 from .ldamulticore import LdaMulticore
 from .phrases import Phrases
